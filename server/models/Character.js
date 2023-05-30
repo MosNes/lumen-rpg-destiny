722427fa70b6c55ba2c370ba12af69954300cb97
MosNes <7hekarl@gmail.com>
2023-05-30 18:33:05 -0400

    added fields to Character.js, added placeholder files for other models

--- server/models/Character.js
+++ server/models/Character.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const characterSchema = new Schema({
+    _id: {
+        type: Schema.Types.ObjectId,
+    },
 
     name: {
         type: String,
@@ -15,6 +18,72 @@ const characterSchema = new Schema({
         required: true,
         trim: true
     },
+
+    class: {
+        type: Schema.Types.ObjectId,
+        ref: 'Class'
+    },
+
+    currentAffinity: {
+        type: Schema.Types.ObjectId,
+        ref: 'Affinity'
+    },
+
+    powers: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Power'
+        }
+    ],
+
+    weapons: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Weapon'
+        }
+    ],
+
+    body: {
+        type: Number,
+        required: true,
+    },
+
+    speed: {
+        type: Number,
+        required: true,
+    },
+
+    mind: {
+        type: Number,
+        required: true,
+    },
+
+    currentHealth: {
+        type: Number,
+    },
+
+    currentMomentum: {
+        type: Number,
+    },
+
+    currentEnergy: {
+        type: Number,
+    },
+
+    anscestry: {
+        type: String,
+        required: true,
+    },
+
+    ghostName: {
+        type: String,
+        required: true,
+    },
+
+    notes: {
+        type: String,
+    }
+
 
 });
 
